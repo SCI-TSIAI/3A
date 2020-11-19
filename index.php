7b29d90551ff7ea2d257ab87db470d2b40baff25
igor.maculewicz <igor.maculewicz@assecods.pl>
2020-11-19 09:03:22 +0100

    Added code for tech exam.

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
 <?php
+//NA EGZAMIN ZAWODOWY TAK!
+//$id = 1;
+//$connection = new PDO("mysql:host=172.17.0.2:3306;dbname=gallery", "root", "root");
+//
+//$query = $connection->query("Select * from user where id=$id");
+//
+//$result = $query->fetch();
+
+
 
 use App\Main;
 
 require __DIR__ . '/vendor/autoload.php';
-
 
 $mainClass = new Main();
 
